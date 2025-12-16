be56be35281fba3e42dc4b61c75ff5435d55b8a0
JuanPabloSE <juancontas@protonmail.com>
2025-12-16 19:37:27 -0300

    Commit Ex02

---
+++ src/Ex04.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            }
+            else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+
+        sc.close();
+    }
+}
