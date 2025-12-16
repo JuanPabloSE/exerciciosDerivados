de948cb89445e54d9045902e0bc1ee1018828f46
JuanPabloSE <juancontas@protonmail.com>
2025-12-16 17:33:54 -0300

    Commit Ex02

---
+++ src/Ex03.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex03 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int soma = 0;
+
+        while (N != 0) {
+            if (N > 0 && N % 2 == 0) {
+                soma += N;
+            }
+            N = sc.nextInt();
+        }
+
+        System.out.println("Soma: " + soma);
+
+        sc.close();
+    }
+}
