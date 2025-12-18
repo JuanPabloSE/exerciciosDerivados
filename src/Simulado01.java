d1a3969a2869702887b14f823a042b82cafbec29
JuanPabloSE <juancontas@protonmail.com>
2025-12-18 10:41:27 -0300

    Commit Ex02

---
+++ src/Simulado01.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Simulado01 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        if (N > 0 && N % 2 == 0) {
+            System.out.println("POSITIVO PAR");
+        }
+
+        else if (N > 0 && N % 2 != 0) {
+            System.out.println("POSITIVO IMPAR");
+        }
+
+        else if (N < 0 && N % 2 == 0) {
+            System.out.println("NEGATIVO PAR");
+        }
+
+        else if (N < 0 && N % 2 != 0) {
+            System.out.println("NEGATIVO IMPAR");
+        }
+
+        else {
+            System.out.println("ZERO");
+        }
+
+        sc.close();
+    }
+}
