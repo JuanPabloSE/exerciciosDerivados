1df53ac8a6b6bccd36aff0e10fa7fdacc312a7c2
JuanPabloSE <juancontas@protonmail.com>
2025-12-26 11:31:48 -0300

    Commit Simulados

---
+++ src/Simulado12.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Simulado12 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 == 0 && i % 3 == 0) {
+                System.out.println("AB");
+            }
+            else if (i % 2 == 0) {
+                System.out.println("A");
+            }
+            else if (i % 3 == 0) {
+                System.out.println("B");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+
+        sc.close();
+    }
+}
