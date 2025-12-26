73590a65f37c17b7d9924e34f874cfef05bf7148
JuanPabloSE <juancontas@protonmail.com>
2025-12-26 11:22:34 -0300

    Commit Simulados

---
+++ src/Simulado11.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Simulado11 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int fatorial = 1;
+
+        for (int i = 1; i <= N; i++) {
+            fatorial *= i;
+        }
+
+        System.out.println(fatorial);
+
+        sc.close();
+    }
+}
