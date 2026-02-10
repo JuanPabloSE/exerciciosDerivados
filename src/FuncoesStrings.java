03035502ec47d049a60b1d2e9e2f6f9b43d65592
JuanPabloSE <juancontas@protonmail.com>
2026-02-10 15:57:37 -0300

    Commit Simulados

--- src/FuncoesStrings.java
+++ src/FuncoesStrings.java
@@ -4,6 +4,20 @@ public class FuncoesStrings {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        String original = "abcde FGHIJ ABC abc DEFEG   ";
+
+        // Funções de Strings em JAVA
+
+        System.out.println("Original: -" + original + "-");
+        String s01 = original.toLowerCase(); // TRANSFORMA TUDO EM LETRAS MINÚSCULAS
+        String s02 = original.toUpperCase(); // TRANSFORMA TUDO EM LETRAS MAIÚSCULAS
+        String s03 = original.trim(); // ELIMINA OS ESPAÇOS NOS CANTOS DA STRING
+
+        System.out.println("toLowerCase: -" + s01 + "-");
+        System.out.println("toUpperCase: -" +s02 + "-");
+        System.out.println("trim: -" + s03 + "-");
+
+
 
 
         sc.close();
