0023b0d176925f244723b7fba35af4b1ab21c111
JuanPabloSE <juancontas@protonmail.com>
2026-03-13 13:23:36 -0300

    Commit Simulados

--- src/Simulado27.java
+++ src/Simulado27.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class Simulado27 {
     public static void main (String [] args) {
+        Scanner sc = new Scanner (System.in);
 
-        for (int i = 0; i < 100
+        int valor = lerValor(sc, "Valor de entrada: ");
+        calcularValor(valor);
+
+        sc.close();
 
     }
+
+    public static int lerValor(Scanner sc, String mensagem) {
+        System.out.print(mensagem);
+        return sc.nextInt();
+    }
+
+    public static void calcularValor(int valor) {
+        for (int i = 0; i < valor; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
 }
